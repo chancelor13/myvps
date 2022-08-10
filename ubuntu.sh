1c01e5e39104c706bdfc08771696e3555d650bb8
chancelor13 <109459606+chancelor13@users.noreply.github.com>
2022-08-10 22:29:41 +0000

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,5 @@
+#Ubuntu.sh
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt dist-upgrade
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev libpq-dev python-psycopg2 python-openssl git nano libjpeg8-dev zlib1g-dev
